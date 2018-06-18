c6212155f7738aa14311d6a79784f31fe3a659fd
Filipe Fenrich <foolemdev@gmail.com>
2018-06-18 16:24:38 -0300

    Escrever readme

--- csmaCd.py
+++ csmaCd.py
@@ -2,105 +2,164 @@ import threading
 import multiprocessing
 import time
 import random
-from UtilCsmaCd import *
+import sys
+from subprocess import os
 
-MAX_LOOP = 10
-MAX_STATION = 3
+MAX_LOOP = 1
+MAX_STATION = 10
 MAX_PACKETS = 5
 TIME_SLOT = 0.0053
-RATE_COLLISION = 10 # rate of collision on cable
-transmissions = [None] * MAX_STATION
+RATE_COLLISION = 25 # rate of collision on cable
 CABLE = [None] * 1 # Cable full = false ,empty = true
 COLLISION = [None] * 1
+
+SUCESS = [0] * MAX_STATION
+FAIL = [0] * MAX_STATION
 
 def csmaCd (station):
     cont = 0
     while cont <= MAX_LOOP:
         cont = cont + 1
-        time.sleep (random.uniform(0.0,2.0))
-        print "ESTACAO %d VERIFICANDO CABO \n" % (station.id)
+        time.sleep (random.uniform(0.0,5.0))
+
+        print ['',"\nESTACAO %d - verificando cabo\n" % (station.id)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+
         if freeCable():
-            print CABLE[0]
-            print "cabo livre para estacao %d" % (station.id)
+
+            print ['',"\nESTACAO %d - utilizando cabo \n" % (station.id)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+
             transmit(station)
             if COLLISION[0]:
                 COLLISION[0] = False
                 CABLE[0] = True
-                resetTransmissions()
-                #backOffTime()
+                backOffTime(station)
         else:
             while  not freeCable():
                 pass
-                #print "estacao %d - esperando liberar cabo " % (station.id)
-                #time.sleep(2)
+
+
+def backOffTime (station):
+    if station.transmissionAttempts <= 10:
+        station.transmissionAttempts += 1
+    timeMax = 2 ** station.transmissionAttempts
+
+    print ['',"ESTACAO %d esperara de 0 a %d slots de tempo\n" % (station.id, timeMax)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+    timeWait = random.uniform(0.0,timeMax)
+
+    print ['',"ESTACAO %d esperara %d slots na tentativa %d\n" % (station.id, timeWait, station.transmissionAttempts)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+    timeWait = timeWait * TIME_SLOT
+    time.sleep(timeWait)
+
 def transmit(station):
-    print "inicio tranmissao da estacao %d" % (station.id)
+
     CABLE[0] = False
-    transmissions[station.id] = True
+    print ['',"ESTACAO %d - iniciando transmissao \n" % (station.id)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+
     causingCollision (station)
-    print transmissions, CABLE[0]
-    randomNumPackets = random.randrange(MAX_PACKETS)
-    print "estacao %d - enviara %d pacotes" % (station.id,randomNumPackets)
+
+    randomNumPackets = random.randrange(1,MAX_PACKETS)
+
+    print ["","ESTACAO %d - enviara %d pacotes\n" % (station.id,randomNumPackets)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+
     for i in range(randomNumPackets):
-        print "estacao %d - enviando pacote %d " % (station.id, i+1)
+        print ["","\t- ESTACAO %d - enviando pacote %d \n" % (station.id, i+1)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
         time.sleep (TIME_SLOT)
+
         if collision():
-            print "##################### COLLISION ############################"
+            print ["","\n< ;-( ESTACAO %d - detectou ### COLISAO ### \n" % (station.id)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+            station.fail += 1
+            FAIL[station.id] = station.fail
+
+            if len(sys.argv) != 1 and sys.argv[1] == '-r':
+                printResul()
+
             return
-    CABLE[0] = True
-    print "estacao %d terminou transmissao com sucesso" % (station.id)
-    transmissions[station.id] = False
+
+    CABLE[0] = True # Estacao terminou transferencia, entao libera o meio fisico
+
+    print ["","\n< :-) ESTACAO %d - terminou transmissao com sucesso \n" % (station.id)][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+    station.sucess += 1
+    SUCESS[station.id] = station.sucess
+    if len(sys.argv) != 1 and sys.argv[1] == '-r':
+        printResul()
+    else:
+        print "\nESTACAO %d transmitiu com SUCESSO " % (station.id)
+
+    station.transmissionAttempts = 0
+
 
 def causingCollision (station):
     luck = random.randrange(1,99)
     if luck <= RATE_COLLISION:
-        transmissions[(station.id + 1) % MAX_STATION] = True
+        COLLISION[0] = True
+
 
 def collision():
-    i = 0
-    for t in transmissions:
-        if t:
-            i = i + 1
-    print "num transmissores %d" % (i)
-    if i > 1:
-        COLLISION[0] = True
     return COLLISION[0]
 
 def freeCable():
     return CABLE[0]
 
+def printResul ():
+    if len(sys.argv) != 1 and sys.argv[1] != '-d':
+        os.system("clear")
+
+    print "\n\n|      #  TRANSMISSOES  #     |"
+    print "| ESTACAO | SUCESSOS | FALHAS |"
+    print "+---------+----------+--------+"
+
+    for i in range(MAX_STATION):
+        print "|    %d    |    %d     |   %d    |" % (i, SUCESS[i], FAIL[i])
+        print "+---------+----------+--------+"
+
+def help():
+    print "Use a linha de comando :"
+    print "python csmaCd.py [ops] "
+    print "Com parametros opcionais, que sao:"
+    print " -d , para modo debug"
+    print " -r , para modo de impressao dos resultados"
+    print " sem utilizar as ops, o programa seguira o enunciado"
+
+
 class Station(object):
     def __init__(self, id):
         self.id = id
-        print self.id
+        self.transmissionAttempts = 0
+        self.sucess = 0
+        self.fail = 0
         csmaCd(self)
-
-def resetTransmissions():
-    for i in range(MAX_STATION):
-        transmissions[i] = False
-    print transmissions
 
 
 def main():
 
-    resetTransmissions()
     COLLISION[0] = False
     CABLE[0] = False
     stations = []
 
-    for i in range(MAX_STATION):
-        station = threading.Thread(target = Station, args = (i,))
-        stations.append(station)
-    for station in stations:
-        station.start()
+    if len(sys.argv) == 1 or (len(sys.argv) > 1 and (sys.argv[1] == '-d' or sys.argv[1] == '-r')):
+
+        for i in range(MAX_STATION):
+            station = threading.Thread(target = Station, args = (i,))
+            stations.append(station)
+
+        for station in stations:
+            station.start()
 
 
-    time.sleep(3)
-    print "\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& cabo livreeeeeee &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-    CABLE[0] = True
+        print ['' ,"Cabo inicia com transmissao de uma mensagem\n"][len(sys.argv) > 1 and (sys.argv[1] == '-d')],
+        time.sleep(3)
 
-    for station in stations:
-        station.join()
+        print (['' ,"&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& CABO LIVRE &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n"][len(sys.argv) > 1 and (sys.argv[1] == '-d')]),
+        CABLE[0] = True
+
+
+        for station in stations:
+            station.join()
+
+        printResul()
+
+    else:
+        help()
 
 if __name__ == '__main__':
     main()
